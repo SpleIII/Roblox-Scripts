8d4aa7ede4b79aae65f24793d2823b34f7ec863b
SpleIII <psxbuy@yandex.ru>
2025-04-09 01:30:25 +0300

    Update SpleIII-ESP-True.lua

--- SpleIII-ESP-True.lua
+++ SpleIII-ESP-True.lua
@@ -1,6 +1,3 @@
---[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
 local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/linemaster2/esp-library/main/library.lua"))();
 
 --// Master switch
