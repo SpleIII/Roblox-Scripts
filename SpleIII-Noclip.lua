3808b99479565d5fae34ef7104f47e44df6780a9
SpleIII <psxbuy@yandex.ru>
2025-04-09 01:02:08 +0300

    Update and rename SpleIII-Noclip to SpleIII-Noclip.lua

--- SpleIII-Noclip.lua
+++ SpleIII-Noclip.lua
@@ -1,7 +1,9 @@
 noclip = false
+getgenv().AddNotification = function(title, text) game:GetService'StarterGui':SetCore("SendNotification", {Title = title; Text = text;}) end
 game:GetService('RunService').Stepped:connect(function()
 if noclip then
 game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
+AddNotification('Читы','Ноуклип - Включён')
 end
 end)
 plr = game.Players.LocalPlayer
@@ -11,5 +13,6 @@ mouse.KeyDown:connect(function(key)
 if key == "n" then
 noclip = not noclip
 game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
+AddNotification('Читы','Ноуклип - Выключен')
 end
 end)
