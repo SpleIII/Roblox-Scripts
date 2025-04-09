39a5f1a8666542b810affbcf8cfd50f9fdaf44f1
SpleIII <psxbuy@yandex.ru>
2025-04-09 03:40:40 +0300

    Update SpleIII-Noclip.lua

--- SpleIII-Noclip.lua
+++ SpleIII-Noclip.lua
@@ -1,18 +1,35 @@
-noclip = false
+getgenv().Players = game:GetService'Players'
+getgenv().Host = Players.LocalPlayer
 getgenv().AddNotification = function(title, text) game:GetService'StarterGui':SetCore("SendNotification", {Title = title; Text = text;}) end
-game:GetService('RunService').Stepped:connect(function()
-if noclip then
-game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
-end
-end)
-AddNotification('Читы','Ноуклип - Включён')
-plr = game.Players.LocalPlayer
-mouse = plr:GetMouse()
-mouse.KeyDown:connect(function(key)
+getgenv().RunService = game:GetService'RunService';
+getgenv().ContextActionService = game:GetService'ContextActionService';
 
-if key == "n" then
-noclip = not noclip
-game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
-end
+local Noclip = true;
+local NoclipKey = 'N';
+AddNotification('Читы','Ноклип - Запущен')
+RunService.RenderStepped:Connect(function()
+    if Noclip then
+        for _,v in pairs(Host.Character:GetDescendants()) do
+            if v:IsA'BasePart' and v.CanCollide then
+                v.CanCollide = false
+            end
+        end
+    else
+        for _,v in pairs(Host.Character:GetDescendants()) do
+            if v:IsA'BasePart' and not v.CanCollide then
+                v.CanCollide = true
+            end
+        end
+    end
 end)
-AddNotification('Читы','Ноуклип - Выключен')
+
+function Noclipping(ActionName:string, Properties:EnumItem)
+    if ActionName == 'Noclip' then
+        if not Properties or Properties == Enum.UserInputState.Begin then
+            Noclip = not Noclip
+            AddNotification('Читы','Ноклип - '..tostring(Noclip))
+        end
+    end
+end
+
+ContextActionService:BindAction('Noclip', Noclipping, true, Enum.KeyCode[NoclipKey])
